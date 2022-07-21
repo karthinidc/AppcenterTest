2d28e95fd9664de674d999bb343eeb9ded1a5e1f
KARTHI <karthi.n@theidcgroup.com>
2022-07-21 14:52:30 +0530

    Initial commit

---
+++ ios/Pods/Target Support Files/Pods-FireDepartment-FireDepartmentTests/Pods-FireDepartment-FireDepartmentTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FireDepartment_FireDepartmentTests : NSObject
+@end
+@implementation PodsDummy_Pods_FireDepartment_FireDepartmentTests
+@end
---
+++ ios/Pods/Target Support Files/Pods-FireDepartment/Pods-FireDepartment-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FireDepartment : NSObject
+@end
+@implementation PodsDummy_Pods_FireDepartment
+@end
